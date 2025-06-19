aca582290399e32616072e2fb558c0358bf9a1d8
yecit1997 <ttorresyecit@gmail.com>
2025-06-18 23:00:30 -0500

    first commit

---
+++ usuario/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class Rol(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombre = models.CharField(max_length=30, unique=True, )
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = 'rol'
+        verbose_name_plural = 'roles'
+        ordering = ['-nombre']
+    
+
+class Usuario(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dni = models.IntegerField(editable=False, unique=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    rol = models.ForeignKey(Rol, on_delete=models.SET_NULL, null=True, blank=True, related_name='usuarios_con_rol')
+    
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+        ordering = ['-first_name']
+    
+    def __str__(self):
+        return self. username
+    
+
+
+
+
