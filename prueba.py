d3d41a67efeffe7d25ea8531c006579a3ef86132
yecit1997 <ttorresyecit@gmail.com>
2025-06-19 22:59:28 -0500

    Creacion de archivo prueba

---
+++ prueba.py
@@ -0,0 +1,31 @@
+'''
+Monotoreo del estado de la CPU y bateria
+'''
+
+import time
+import psutil
+
+
+def convertir_tiempo(segundos):
+    minutos, segundos = divmod(segundos, 60)
+    horas, minutos = divmod(minutos, 60)
+    
+    return "%d: %02d: %02d" % (horas, minutos, segundos)
+
+while True:
+    bateria = psutil.sensors_battery()
+    print(psutil.virtual_memory())
+    # print('Bateria: ',bateria)
+    
+    uso_cpu = psutil.cpu_percent(interval=1)
+    print(f'Uso de CPU: {uso_cpu}')
+    
+    time.sleep(1)
+    
+    if bateria is not None:
+        print('Porcentaje de bateria: ', bateria.percent)
+        print('conectado a la corriente: ', bateria.power_plugged)
+        print('Tiempo restante de bateria: ', bateria.secsleft)
+        
+
+
